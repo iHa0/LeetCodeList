d933bbf2fd46e8fd6fd8ab21c26e8b6a6429cc59
iHao <sinhao@live.com>
2021-05-27 17:10:31 +0800

    LeetCode116【树操作-填充每个节点的下一个右侧节点指针】（中等）

---
+++ LeetCode116.java
@@ -0,0 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class LeetCode116 {
+    public Node connect(Node root) {
+        Node node = new Node();
+        if (root == null){
+            return null;
+        }
+        if (root.left == null && root.right == null)
+            root.next = null;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            Node p = queue.poll();
+            Node q = new Node();
+            for (int i = 0; i < size; i++) {
+                if (queue.isEmpty() || i == size -1){
+                    p.next = null;
+                }else {
+                    q = queue.poll();
+                    p.next = q;
+                }
+                if (p.left != null){
+                    queue.add(p.left);
+                }
+                if (p.right != null){
+                    queue.add(p.right);
+                }
+                p = q;
+            }
+        }
+        return root;
+    }
+}
