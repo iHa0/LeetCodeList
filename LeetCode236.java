880ee67a2410f48760e049fb07db019d5db2dc6c
iHao <sinhao@live.com>
2021-06-09 18:00:27 +0800

    LeetCode236【树操作-公共祖先】（中等）

---
+++ LeetCode236.java
@@ -0,0 +1,15 @@
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        //如果left为空，说明这两个节点在cur结点的右子树上，我们只需要返回右子树查找的结果即可
+        if (left == null)
+            return right;
+        //同上
+        if (right == null)
+            return left;
+        //如果left和right都不为空，说明这两个节点一个在cur的左子树上一个在cur的右子树上，
+        //我们只需要返回cur结点即可。
+        return root;
+    }
