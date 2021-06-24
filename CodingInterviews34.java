2d3bc59186cf0c8e62107b8888c35e120ec28058
iHao <sinhao@live.com>
2021-06-24 20:34:28 +0800

    CodingInterviews34【剑指Offer-二叉树中和为某一值的路径】（中等）

---
+++ CodingInterviews34.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class CodingInterviews34 {
+    List<List<Integer>> res = new ArrayList<>();
+    Deque<Integer> deque = new ArrayDeque<>();
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null){
+            return list;
+        }
+        dfs(root, targetSum);
+        return res;
+    }
+    public void dfs(TreeNode root , int targetSum){
+        if (root == null){
+            return ;
+        }
+        deque.addLast(root.val);
+        int sum = targetSum - root.val;
+        if (sum == 0 && root.left == null && root.right == null){
+            res.add(new ArrayList<>(deque));
+        }
+        dfs(root.left , sum);
+        dfs(root.right , sum);
+        deque.pollLast();
+    }
+}
