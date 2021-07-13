d916a2f90385640feb579bd99c5b572cbf5414e5
iHao <sinhao@live.com>
2021-07-13 21:33:06 +0800

    LeetCode174【动态规划-买卖股票的最佳时机含手续费】（中等）

---
+++ LeetCode174.java
@@ -0,0 +1,13 @@
+public class LeetCode174 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
