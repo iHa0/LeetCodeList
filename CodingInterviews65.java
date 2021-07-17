8da1320e40541e662d6d645866dc8a82be98a228
iHao <sinhao@live.com>
2021-07-17 21:49:52 +0800

    CodingInterviews65【剑指Offer-不用加减乘除做加法】（简单）

---
+++ CodingInterviews65.java
@@ -0,0 +1,10 @@
+public class CodingInterviews65 {
+    public int add(int a, int b) {
+        while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
