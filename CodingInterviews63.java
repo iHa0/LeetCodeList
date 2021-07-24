36a6e293b123ccd8adf1b7de4cb6b72f635580be
iHao <sinhao@live.com>
2021-07-24 21:17:43 +0800

    CodingInterviews63【剑指Offer-股票的最大利润】（中等）

---
+++ CodingInterviews63.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class CodingInterviews63 {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len <= 1)
+            return 0;
+        int[] dp = new int[len];
+        dp[0] = 0;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] - prices[i - 1] <= 0){
+                dp[i] = Math.max(0, prices[i] - prices[i - 1] + dp[i - 1]);
+            }else {
+                dp[i] = prices[i] - prices[i - 1] + dp[i - 1];
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
