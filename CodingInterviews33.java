b28ed099d0e4e20c157d715d506f0294709b4fc2
iHao <sinhao@live.com>
2021-07-28 21:39:25 +0800

    CodingInterviews33【剑指Offer-二叉搜索树的后序遍历序列】（中等）

---
+++ CodingInterviews33.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class CodingInterviews33 {
+    public boolean verifyPostorder(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for(int i = postorder.length - 1; i >= 0; i--) {
+            if(postorder[i] > root) return false;
+            while(!stack.isEmpty() && stack.peek() > postorder[i])
+                root = stack.pop();
+            stack.add(postorder[i]);
+        }
+        return true;
+    }
+}
