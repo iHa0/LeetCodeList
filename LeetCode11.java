4184b331693b1bf715ae85fe4e03b3e12cd68724
iHao <sinhao@live.com>
2021-07-30 21:33:30 +0800

    LeetCode11【动态规划-盛最多水的容器】（中等）

---
+++ LeetCode11.java
@@ -0,0 +1,11 @@
+public class LeetCode11 {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j){
+            res = height[i] < height[j] ?
+                    Math.max(res, (j - i) * height[i++]):
+                    Math.max(res, (j - i) * height[j--]);
+        }
+        return res;
+    }
+}
