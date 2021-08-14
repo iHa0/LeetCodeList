507ea3f4e9d2563eeb881149132738ab58776d4e
iHao <sinhao@live.com>
2021-08-14 20:15:42 +0800

    LeetCode35【数组操作-搜索插入位置】（简单）

---
+++ LeetCode35.java
@@ -0,0 +1,10 @@
+public class LeetCode35 {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i] >= target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
