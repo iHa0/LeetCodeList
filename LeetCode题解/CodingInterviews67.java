03dc7a955f976bcb50a8727849becb72533f6f29
iHao <sinhao@live.com>
2021-08-21 21:49:50 +0800

    CodingInterviews67【二进制操作-二进制求和】（简单）

---
+++ LeetCode题解/CodingInterviews67.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+}
