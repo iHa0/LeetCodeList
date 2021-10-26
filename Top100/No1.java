e01f059e2d372618b1452bde175e28e53640bec2
iHao <sinhao@live.com>
2021-10-26 16:08:55 +0800

    Top100-1/100

--- Top100/No1.java
+++ Top100/No1.java
@@ -1,5 +1,16 @@
-public class No1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
+import java.util.Arrays;
+
+public class Top100_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
     }
 }
