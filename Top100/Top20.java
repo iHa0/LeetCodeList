67ef51e5c153a853a28e1f1bdece5c86959917f5
iHao <sinhao@live.com>
2021-11-03 21:51:46 +0800

    Top20/100

--- Top100/Top20.java
+++ Top100/Top20.java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 == 1)
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+                continue;
+            }
+            if (c == ')'){
+                if (!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                    continue;
+                }
+                stack.push(c);
+            }
+            if (c == ']'){
+                if (!stack.isEmpty() && stack.peek() == '['){
+                    stack.pop();
+                    continue;
+                }
+                stack.push(c);
+            }if (c == '}'){
+                if (!stack.isEmpty() && stack.peek() == '{'){
+                    stack.pop();
+                    continue;
+                }
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
