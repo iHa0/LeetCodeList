85983a551862789dd093297bc82b4f0dce3132bf
iHao <sinhao@live.com>
2021-12-02 17:15:48 +0800

    Top238/100

---
+++ Top100/Top238.java
@@ -0,0 +1,17 @@
+package Top100;
+
+public class Top238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int p = 1, q = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = p;
+            p *= nums[i];
+        }
+        for (int i = nums.length - 1; i > 0; i--) {
+            q *= nums[i];
+            res[i - 1] *= q;
+        }
+        return res;
+    }
+}
