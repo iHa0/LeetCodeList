b425c162622e635523299438115c00b1f419aa9e
iHao <sinhao@live.com>
2021-12-07 18:55:55 +0800

    Top309/100

---
+++ Top100/Top309.java
@@ -0,0 +1,17 @@
+package Top100;
+
+public class Top309 {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[1][0] = Math.max(dp[0][0], dp[0][1] + prices[1]);
+        dp[1][1] = Math.max(dp[0][1], -prices[1]);
+        for (int i = 2; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+}
