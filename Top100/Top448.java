75453ad9cb15db0bc4d3a04c38fe6c97474611cf
iHao <sinhao@live.com>
2021-12-12 21:16:42 +0800

    Top448.java

---
+++ Top100/Top448.java
@@ -0,0 +1,23 @@
+package Top100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Top448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> res = new ArrayList<>();
+        int len = nums.length;
+        int[] temp = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            int n = nums[i];
+            temp[n]++;
+        }
+        for (int i = 1; i < len + 1; i++) {
+            if (temp[i] == 0)
+                res.add(i);
+        }
+        return res;
+    }
+}
