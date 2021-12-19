757f0b5b972cdea45bf0b57b288b30fa44838cdd
iHao <sinhao@live.com>
2021-12-19 16:07:19 +0800

    TopInterview11/145

---
+++ TopInterview题解/TopInterview11.java
@@ -0,0 +1,13 @@
+package TopInterview;
+
+public class TopInterview11 {
+    public int maxArea(int[] height) {
+        int max = Integer.MIN_VALUE;
+        int len = height.length;
+        int left = 0, right = len - 1;
+        while (left < right){
+            max = Math.max(max, height[left] > height[right] ? (right - left) * height[right--] : (right - left) * height[left++]);
+        }
+        return max;
+    }
+}
