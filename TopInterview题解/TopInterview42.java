4d13f222a150286cd2bec9bdcdc5876e3d2e57dc
iHao <sinhao@live.com>
2021-12-22 15:15:43 +0800

    TopInterview42/145

---
+++ TopInterview题解/TopInterview42.java
@@ -0,0 +1,25 @@
+package TopInterview;
+
+public class TopInterview42 {
+    public int trap(int[] height) {
+        int len = height.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = height[0];
+        right[len - 1] = height[len - 1];
+        for (int i = 1; i < len; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = Math.max(height[i], right[i + 1]);
+        }
+        for (int i = 0; i < len; i++) {
+            left[i] = Math.min(left[i], right[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res += left[i] - height[i];
+        }
+        return res;
+    }
+}
