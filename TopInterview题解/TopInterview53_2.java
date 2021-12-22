0f54277834672643017d6072cc171c64e50f7b27
iHao <sinhao@live.com>
2021-12-22 17:57:34 +0800

    TopInterview53/100(小改进)

---
+++ TopInterview题解/TopInterview53_2.java
@@ -0,0 +1,15 @@
+package TopInterview;
+
+public class TopInterview53 {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
