28d33e76645b394965f24e87274de1dc850f7d0a
iHao <sinhao@live.com>
2021-12-23 19:41:58 +0800

    TopInterview69/145

---
+++ TopInterview题解/TopInterview69.java
@@ -0,0 +1,17 @@
+package TopInterview;
+
+public class TopInterview69 {
+    public int mySqrt(int x) {
+        int left = 0, right = x, res = -1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x){
+                res = mid;
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return res;
+    }
+}
