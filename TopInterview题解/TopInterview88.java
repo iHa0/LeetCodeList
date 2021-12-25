4e2477c602539af0cb719a2b70de11aa94709108
iHao <sinhao@live.com>
2021-12-25 16:49:42 +0800

    TopInterview88/145

---
+++ TopInterview题解/TopInterview88.java
@@ -0,0 +1,12 @@
+package TopInterview;
+
+import java.util.Arrays;
+
+public class TopInterview88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[i - m];
+        }
+        Arrays.sort(nums1);
+    }
+}
