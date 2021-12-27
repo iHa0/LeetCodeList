f3baa384599e64ec2969630fe95c6c54297f5b9a
iHao <sinhao@live.com>
2021-12-27 16:07:58 +0800

    TopInterview104/145

---
+++ TopInterview题解/TopInterview104.java
@@ -0,0 +1,9 @@
+package TopInterview;
+
+public class TopInterview104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
