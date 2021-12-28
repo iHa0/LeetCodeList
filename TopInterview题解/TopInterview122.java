7d5af888dcb1ef275d1d0de7773bf74a5c779b76
iHao <sinhao@live.com>
2021-12-28 16:36:56 +0800

    TopInterview122/145[Stock problem②]

---
+++ TopInterview题解/TopInterview122.java
@@ -0,0 +1,15 @@
+package TopInterview;
+
+public class TopInterview122 {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+}
