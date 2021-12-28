da1234fcd257c2b342e8d9608ab91fbb5ef7242c
iHao <sinhao@live.com>
2021-12-28 16:58:43 +0800

    TopInterview123/145[Stock problem③]

---
+++ TopInterview题解/TopInterview123.java
@@ -0,0 +1,20 @@
+package TopInterview;
+
+public class TopInterview123 {
+    public int maxProfit(int[] prices) {
+        int k = 2;
+        int len = prices.length;
+        int[][][] dp = new int[len][k + 1][2];
+        dp[0][1][0] = 0;
+        dp[0][2][0] = 0;
+        dp[0][1][1] = -prices[0];
+        dp[0][2][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j <= k; j++) {
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+        return dp[len - 1][k][0];
+    }
+}
