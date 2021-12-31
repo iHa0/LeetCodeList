aeacdaeeef7645e7adfebd28ebdeb1233e7a3acb
iHao <sinhao@live.com>
2021-12-31 11:12:29 +0800

    TopInterview136/145

---
+++ TopInterview题解/TopInterview136.java
@@ -0,0 +1,11 @@
+package TopInterview;
+
+public class TopInterview136 {
+    public int singleNumber(int[] nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            nums[0] ^= nums[i];
+        }
+        return nums[0];
+    }
+}
