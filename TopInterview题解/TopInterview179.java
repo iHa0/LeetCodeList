6f1bdecf494adbc00bbbd6b0e924b6ca1eb842fa
iHao <sinhao@live.com>
2022-01-07 20:47:27 +0800

    TopInterview179

---
+++ TopInterview题解/TopInterview179.java
@@ -0,0 +1,24 @@
+package TopInterview;
+
+import java.util.Arrays;
+
+public class TopInterview179 {
+    public String largestNumber(int[] nums) {
+        int len = nums.length;
+        String[] ss = new String[len];
+        for (int i = 0; i < len; i++) {
+            ss[i] = nums[i] + "";
+        }
+        Arrays.sort(ss, (o1, o2) -> {
+            String ab = o1 + o2, ba = o2 + o1;
+            return ba.compareTo(ab);
+        });
+        String res = "";
+        for (int i = 0; i < len; i++) {
+            if (ss[0].equals("0"))
+                return "0";
+            res += ss[i];
+        }
+        return res;
+    }
+}
