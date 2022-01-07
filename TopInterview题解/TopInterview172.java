dabe0d339b359c13cc992f9ba92b3554eb4df342
iHao <sinhao@live.com>
2022-01-07 20:05:17 +0800

    TopInterview172/145★★★★☆

---
+++ TopInterview题解/TopInterview172.java
@@ -0,0 +1,12 @@
+package TopInterview;
+
+public class TopInterview172 {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0){
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
