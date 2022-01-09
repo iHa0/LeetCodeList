1159cca06e18c54c285e1ea2768068b99a570ec6
iHao <sinhao@live.com>
2022-01-09 18:28:39 +0800

    TopInterview190/145

---
+++ TopInterview题解/TopInterview190.java
@@ -0,0 +1,12 @@
+package TopInterview;
+
+public class TopInterview190 {
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
