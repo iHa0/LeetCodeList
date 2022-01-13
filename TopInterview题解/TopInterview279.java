cb5d23a6d7eb1545dd91c312d17a966855605aef
iHao <sinhao@live.com>
2022-01-13 16:01:52 +0800

    TopInterview279/145★★★★☆

---
+++ TopInterview题解/TopInterview279.java
@@ -0,0 +1,15 @@
+package TopInterview;
+
+public class TopInterview279 {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
