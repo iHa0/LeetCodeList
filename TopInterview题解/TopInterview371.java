a7ad6e5a3fa99646c8db139c04fce69f7e6ce96a
iHao <sinhao@live.com>
2022-01-21 16:03:55 +0800

    TopInterview371/145

---
+++ TopInterview题解/TopInterview371.java
@@ -0,0 +1,12 @@
+package TopInterview;
+
+public class TopInterview371 {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
