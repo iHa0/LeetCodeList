d936053ee51540390ff950e06ef19fc2a84aa6b9
iHao <sinhao@live.com>
2022-03-04 23:32:44 +0800

    LeetCode2104

---
+++ LeetCode题解/LeetCode2104.java
@@ -0,0 +1,15 @@
+public class LeetCode2104 {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            int min = nums[i], max = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+                ans += max - min;
+            }
+        }
+        return ans;
+    }
+}
