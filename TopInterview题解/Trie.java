23ba0db897e4676c103b29c746c469ec34568c4a
iHao <sinhao@live.com>
2022-03-17 18:30:27 +0800

    LeetCode208

---
+++ TopInterview题解/Trie.java
@@ -0,0 +1,48 @@
+public class Trie {
+    class TrieNode {
+        private boolean isEnd;
+        TrieNode[] next;
+
+        public TrieNode() {
+            isEnd = false;
+            next = new TrieNode[26];
+        }
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node = node.next[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null)
+                return false;
+        }
+        return node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null)
+                return false;
+        }
+        return true;
+    }
+}
