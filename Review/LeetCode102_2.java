cfd4ebef3dba648133d0b0f9170fc22df5f37ff4
iHao <sinhao@live.com>
2022-03-23 00:18:10 +0800

    LeetCode102

---
+++ Review/LeetCode102_2.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode p = queue.poll();
+                list.add(p.val);
+                if (p.left != null) {
+                    queue.add(p.left);
+                }
+                if (p.right != null) {
+                    queue.add(p.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
