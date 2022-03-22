5ac738e2392d72ecbf2464869f8c90340383ac67
iHao <sinhao@live.com>
2022-03-23 00:15:38 +0800

    LeetCode101

---
+++ Review/LeetCode101.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return false;
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return dfs(p.left, q.right) && dfs(p.right, q.left);
+    }
+}
