8627eee8a5d5316abca3807a2d231d37f9200da9
iHao <sinhao@live.com>
2022-03-28 01:00:18 +0800

    LeetCode486【预测赢家】（中等）

---
+++ LeetCode题解/LeetCode486_2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int len = nums.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][len - 1] >= 0;
+    }
+}
