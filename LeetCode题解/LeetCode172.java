79b3e9e6ac34c2278af40c8edee7fb0164aa92c0
iHao <sinhao@live.com>
2022-03-28 01:14:54 +0800

    LeetCode172【阶乘后的零】（中等）

---
+++ LeetCode题解/LeetCode172.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+}
