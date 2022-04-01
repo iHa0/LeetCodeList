33a2056017549f5df37c9d9ca36349dbafacecd6
iHao <sinhao@live.com>
2022-04-01 20:50:49 +0800

    LeetCode55

---
+++ Review/Top100/LeetCode55.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        int flag = nums[0];
+        for (int i = 0; i < len; i++) {
+            if (i <= flag) {
+                flag = Math.max(flag, i + nums[i]);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
