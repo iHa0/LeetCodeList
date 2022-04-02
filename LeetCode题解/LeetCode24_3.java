7183f392613c469e4432e4daabe74563e91ba82a
iHao <sinhao@live.com>
2022-04-02 22:14:10 +0800

    LeetCode24

---
+++ LeetCode题解/LeetCode24_3.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = head.next;
+        head.next = swapPairs(p.next);
+        p.next = head;
+        return p;
+    }
+}
