bfbeef5edb9d52a25450715f00fdb9e642853615
iHao <sinhao@live.com>
2022-04-02 20:52:17 +0800

    LeetCode994

---
+++ Review/LeetCode994.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int h = grid.length, l = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < l; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        int step = 0;
+        while (count > 0 && !queue.isEmpty()) {
+            int size = queue.size();
+            step++;
+            for (int i = 0; i < size; i++) {
+                int[] orange = queue.poll();
+                int h1 = orange[0], l1 = orange[1];
+                if (h1 - 1 >= 0 && grid[h1 - 1][l1] == 1) {
+                    grid[h1 - 1][l1] = 2;
+                    count--;
+                    queue.add(new int[]{h1 - 1, l1});
+                }
+                if (h1 + 1 < h && grid[h1 + 1][l1] == 1) {
+                    grid[h1 + 1][l1] = 2;
+                    count--;
+                    queue.add(new int[]{h1 + 1, l1});
+                }
+                if (l1 - 1 >= 0 && grid[h1][l1 - 1] == 1) {
+                    grid[h1][l1 - 1] = 2;
+                    count--;
+                    queue.add(new int[]{h1, l1 - 1});
+                }
+                if (l1 + 1 < l && grid[h1][l1 + 1] == 1) {
+                    grid[h1][l1 + 1] = 2;
+                    count--;
+                    queue.add(new int[]{h1, l1 + 1});
+                }
+            }
+        }
+        if (count > 0) return -1;
+        return step;
+    }
+}
