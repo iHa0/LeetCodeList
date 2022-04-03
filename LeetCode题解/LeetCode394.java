082039279953e49b3a581d43460416dd82a19ca0
iHao <sinhao@live.com>
2022-04-03 23:07:27 +0800

    LeetCode394【字符串解码】（中等）

---
+++ LeetCode题解/LeetCode394.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String decodeString(String s) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        LinkedList<Integer> stackMulti = new LinkedList<>();
+        LinkedList<String> stackRes = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                stackMulti.addLast(multi);
+                stackRes.addLast(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            }
+            else if (c == ']') {
+                StringBuilder temp = new StringBuilder();
+                int curMulti = stackMulti.removeLast();
+                for (int i = 0; i < curMulti; i++) temp.append(res);
+                res = new StringBuilder(stackRes.removeLast() + temp);
+            }
+            else if (c >= '0' && c <= '9') {
+                multi = multi * 10 + Integer.parseInt(c + "");
+            }
+            else res.append(c); 
+        }
+        return res.toString();
+    }
+}
