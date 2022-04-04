be17538884c83e2ae8fa20c6cce58f0a82a6c460
iHao <sinhao@live.com>
2022-04-04 20:29:41 +0800

    LeetCode178【分数排名】（中等）

---
+++ LeetCode题解/LeetCode178.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select score, (
+    select count(distinct b.score) 
+    from Scores b
+    where b.score >= a.score
+) as 'rank'
+from Scores a
+order by score desc
