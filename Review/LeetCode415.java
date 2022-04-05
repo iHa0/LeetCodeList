67aa887e8b308314b12e2878e4cf23c49c9689f8
iHao <sinhao@live.com>
2022-04-05 22:29:05 +0800

    LeetCode415

---
+++ Review/LeetCode415.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int temp = n1 + n2 + carry;
+            carry = temp / 10;
+            sb.append(temp % 10);
+            i--;
+            j--;
+        }
+        if (carry == 1) sb.append(1);
+        return sb.reverse().toString();
+    }
+}
