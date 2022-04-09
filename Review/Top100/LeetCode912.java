08e08d1587c11c422dd98973afad49d181bbebb6
iHao <sinhao@live.com>
2022-04-09 20:38:03 +0800

    LeetCode912

---
+++ Review/Top100/LeetCode912.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        quickSort(nums, 0, len - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = sort(nums, left, right);
+            quickSort(nums, left, mid - 1);
+            quickSort(nums, mid + 1, right);
+        }
+    }
+
+    private int sort(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] >= pivot) right--;
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot) left++;
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
+    }
+}
