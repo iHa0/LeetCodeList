9a1a9a0aaa117ccd5bb3d566ceee271a177abd1f
iHao <sinhao@live.com>
2022-04-10 22:07:58 +0800

    LeetCode142

---
+++ Review/Top100/LeetCode142.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode p = head, q = head;
+        while (p != null && q != null && q.next != null) {
+            p = p.next;
+            q = q.next.next;
+            if (p == q) {
+                p = head;
+                while (p != q) {
+                    p = p.next;
+                    q = q.next;
+                }
+                return p;
+            }
+        }
+        return null;
+        
+    }
+}
