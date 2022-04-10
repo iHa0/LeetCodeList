e63f4ee3ad4c69b69b4c1479159a92480d98e06e
iHao <sinhao@live.com>
2022-04-10 23:54:03 +0800

    LeetCode704

---
+++ Review/Top100/LeetCode704.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
