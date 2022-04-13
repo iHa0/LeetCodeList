781a304d200dd223d4206fc7ba26e00e818a9afd
iHao <sinhao@live.com>
2022-04-13 22:36:12 +0800

    LeetCode172

---
+++ Review/LeetCode172.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while (n > 0) {
+            n /= 5;
+            res += n;
+        }
+        return res;
+    }
+}
