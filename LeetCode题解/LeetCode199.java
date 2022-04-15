2ed0671b8d40af2958363e46e326bcc9e81c33b9
iHao <sinhao@live.com>
2022-04-16 00:24:40 +0800

    LeetCode199【二叉树的右视图】（中等）

---
+++ LeetCode题解/LeetCode199.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode p = queue.poll();
+                if (i == size - 1) {
+                    res.add(p.val);
+                }
+                if (p.left != null) queue.add(p.left);
+                if (p.right != null) queue.add(p.right);
+            }
+        }
+        return res;
+    }
+}
