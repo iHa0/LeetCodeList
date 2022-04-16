9d96a2d6bbad8533e07228c50acb276f84fb8afc
iHao <sinhao@live.com>
2022-04-16 21:29:17 +0800

    LeetCode42

---
+++ LeetCode题解/LeetCode42_2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        int len = height.length, res = 0;
+        int left = 0, right = len - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) leftMax = height[left];
+                else res += leftMax - height[left];
+                left++;
+            } else {
+                if (height[right] >= rightMax) rightMax = height[right];
+                else res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
