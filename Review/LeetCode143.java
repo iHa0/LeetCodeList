522072ce8a0d4a65435cc4022054e2a34b8fa4a9
iHao <sinhao@live.com>
2022-04-16 21:56:36 +0800

    LeetCode143

---
+++ Review/LeetCode143.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode temp = slow.next;
+        slow.next = null;
+        ListNode head2 = reverseList(temp);
+        ListNode p = head, nxt = head, newnxt = head2;
+        while (newnxt != null) {
+            nxt = nxt.next;
+            newnxt = newnxt.next;
+            p.next = head2;
+            head2.next = nxt;
+            p = nxt;
+            head2 = newnxt;
+        }
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
