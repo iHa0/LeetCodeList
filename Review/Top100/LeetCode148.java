d48f95841dca6ff6d36f906837ad3bd73d7e6416
iHao <sinhao@live.com>
2022-04-19 00:05:58 +0800

    LeetCode148

---
+++ Review/Top100/LeetCode148.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode h2 = slow.next;
+        slow.next = null;
+        return merge(sortList(head), sortList(h2));
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+ }
