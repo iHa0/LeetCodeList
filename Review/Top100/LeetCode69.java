72ac5f93ba1600a3406ffef2c189b33fab8e6932
iHao <sinhao@live.com>
2022-04-19 21:40:04 +0800

    LeetCode69

---
+++ Review/Top100/LeetCode69.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        int[] tails = new int[len];
+        int res = 0;
+        for (int num : nums) {
+            int left = 0, right = res;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < num) left = mid + 1;
+                else right = mid;
+            }
+            tails[left] = num;
+            if (res == right) res++;
+        }
+        return res;
+    }
+}
