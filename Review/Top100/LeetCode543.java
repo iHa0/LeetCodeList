3e7aa53f3fcc138c6dd0add2aa5a48e07d69aa7a
iHao <sinhao@live.com>
2022-04-24 00:38:56 +0800

    LeetCode543

---
+++ Review/Top100/LeetCode543.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return res;
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        res = Math.max(res, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
