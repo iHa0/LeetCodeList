224e8d9cd016f8cd0fff523a041a1c9ec0fe148b
iHao <sinhao@live.com>
2022-04-25 21:49:05 +0800

    LeetCode113

---
+++ Review/Top100/LeetCode113.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    int target;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        this.target = targetSum;
+        LinkedList<Integer> list = new LinkedList<>();
+        dfs(root, list, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode root, LinkedList<Integer> list, int sum) {
+        if (root == null) return;
+        list.add(root.val);
+        sum += root.val;
+        if (root.left == null && root.right == null) {
+            if (sum == target) {
+                res.add(new ArrayList<>(list));
+            }
+        }
+        dfs(root.left, list, sum);
+        dfs(root.right, list, sum);
+        list.removeLast();
+    }
+}
