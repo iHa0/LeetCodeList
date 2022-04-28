4b6431673c3bbda677503fc2fd2cb46f88cbdba4
iHao <sinhao@live.com>
2022-04-29 00:23:14 +0800

    LeetCode98

---
+++ Review/LeetCode98.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        if (!isValidBST(root.left)) return false;
+        if (pre  >= root.val) return false;
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
