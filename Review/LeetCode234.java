fe0a799f731d59a7222c32f78dc53dfd0dc32547
iHao <sinhao@live.com>
2022-05-01 20:56:17 +0800

    LeetCode234

---
+++ Review/LeetCode234.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode fast = head, slow = head;
+        ListNode pre = head, prepre = null;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            pre.next = prepre;
+            prepre = pre;
+        }
+        if (fast != null) slow = slow.next;
+        while (pre != null && slow != null) {
+            if (pre.val != slow.val) return false;
+            pre = pre.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
