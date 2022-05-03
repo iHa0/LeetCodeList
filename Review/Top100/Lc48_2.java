dd59fd2fe0ca14d314f5222ca0de8e521107a549
iHao <sinhao@live.com>
2022-05-03 23:35:06 +0800

    LeetCode48

---
+++ Review/Top100/Lc48_2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = i; j < len - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j - 1][i];
+                matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+                matrix[j][len - i - 1] = temp;
+            }
+        }
+    }
+}
