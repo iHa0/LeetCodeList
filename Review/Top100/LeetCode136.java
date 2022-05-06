daeb58a287543e850295bbc20cd1c48b153bd688
iHao <sinhao@live.com>
2022-05-06 22:18:17 +0800

    LeetCode136

---
+++ Review/Top100/LeetCode136.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            nums[0] ^= nums[i];
+        }
+        return nums[0];
+    }
+}
