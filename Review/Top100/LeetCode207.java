a86abc883317c15519aaf1ef77e26eb570717aaa
iHao <sinhao@live.com>
2022-05-12 23:29:14 +0800

    LeetCode207[Solution 1]

---
+++ Review/Top100/LeetCode207.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+        for (int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) queue.add(i);
+        }
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for (int cur : adjacency.get(pre)) {
+                if (--indegrees[cur] == 0) queue.add(cur);
+            }
+        }
+        return numCourses == 0;
+    }
+}
