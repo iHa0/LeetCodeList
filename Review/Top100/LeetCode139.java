c5a6c859d7a9eedfef18ac820d1f9458b4c8f935
iHao <sinhao@live.com>
2022-05-13 21:03:57 +0800

    LeetCode139

---
+++ Review/Top100/LeetCode139.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] == true && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
+}
