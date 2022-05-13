b4c83e662503718063781d964f9e8701ca70889a
iHao <sinhao@live.com>
2022-05-13 22:25:59 +0800

    LeetCode416

---
+++ Review/Top100/LeetCode416.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int len = nums.length;
+        int sum = 0;
+        for (int a : nums) sum += a;
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int a : nums) {
+            for (int i = target; i >= a; i--) {
+                dp[i] = dp[i] || dp[i - a];
+            }
+        }
+        return dp[target];
+    }
+}
