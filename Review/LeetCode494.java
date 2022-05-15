cc214af7f7a80012a49140a5de66a19298509dec
iHao <sinhao@live.com>
2022-05-15 23:11:38 +0800

    LeetCode494

---
+++ Review/LeetCode494.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int len = nums.length, sum = 0;
+        for (int a : nums) sum += a;
+        if (Math.abs(sum) < Math.abs(target) || (sum + target) % 2 == 1) return 0;
+        int tar = (sum + target) / 2;
+        int[] dp = new int[tar + 1];
+        dp[0] = 1;
+        for (int a : nums) {
+            for (int i = tar; i >= a; i--) {
+                dp[i] += dp[i - a];
+            }
+        }
+        return dp[tar];
+    }
+}
