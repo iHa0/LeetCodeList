c210a65e9433ddaf145a163c9811e59c6c189f90
iHao <sinhao@live.com>
2022-05-16 23:54:04 +0800

    LeetCode279

---
+++ Review/LeetCode279.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= i * i) {
+                    dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
