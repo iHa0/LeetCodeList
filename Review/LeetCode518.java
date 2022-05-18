5384460588f13d5a965e97f03c9fc86598875dfe
iHao <sinhao@live.com>
2022-05-18 21:32:15 +0800

    LeetCode518

---
+++ Review/LeetCode518.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int len = coins.length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
