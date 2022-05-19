3bda428881f123ece98e146eabbf417a4f5c339c
iHao <sinhao@live.com>
2022-05-19 20:44:18 +0800

    LeetCode139

---
+++ Review/Lc139.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (String a : wordDict) {
+                int size = a.length();
+                if (i - size >= 0 && s.substring(i - size, i).equals(a)) {
+                    dp[i] = dp[i] || dp[i - size];
+                }
+            }
+        }
+        return dp[len];
+    }
+}
