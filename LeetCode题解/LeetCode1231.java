7d6f6e4d0a757975f1dd4d560adcf4b623aa4ae0
iHao <sinhao@live.com>
2022-05-19 21:36:05 +0800

    LeetCode1231【分享巧克力】（困难）

---
+++ LeetCode题解/LeetCode1231.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximizeSweetness(int[] sweetness, int k) {
+        int left = 1, right = Integer.MAX_VALUE;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (check(sweetness, k + 1, mid)) left = mid + 1;
+            else right = mid;
+        }
+        return left - 1;
+    }
+
+    private boolean check(int[] sweetness, int k, int mid) {
+        int sum = 0, count = 0;
+        for (int a : sweetness) {
+            sum += a;
+            if (sum >= mid) {
+                sum = 0;
+                count++;
+            }
+        }
+        return count >= k;
+    }
+}
