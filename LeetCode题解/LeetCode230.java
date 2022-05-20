6f2dfbeb5d991d6d8a0293336160e4a8c642173f
iHao <sinhao@live.com>
2022-05-20 23:52:33 +0800

    LeetCode230【二叉搜索树中第K小的元素】（中等）

---
+++ LeetCode题解/LeetCode230.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int k, res;
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) return 0;
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        if (k == 0) return;
+        if (--k == 0) res = root.val;
+        dfs(root.right);
+    }
+}
