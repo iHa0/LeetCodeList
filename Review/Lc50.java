52cbf2380987be6498df113a1c337ed4dcb8b579
iHao <sinhao@live.com>
2022-05-21 21:22:14 +0800

    LeetCode50

---
+++ Review/Lc50.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return n > 0 ? dfs(x, n) : 1 / dfs(x, n);
+    }
+
+    private double dfs(double x, int n) {
+        if (n == 0) return 1.0;
+        double res = dfs(x, n / 2);
+        return n % 2 == 0 ? res * res : x * res * res;
+    }
+}
