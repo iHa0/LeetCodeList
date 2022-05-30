6f383c19f2ec1d472f1c80b3a8155faa38c6ef39
iHao <sinhao@live.com>
2022-05-30 21:39:29 +0800

    LeetCode450【删除二叉搜索树中的节点】（中等）

---
+++ LeetCode题解/LeetCode450.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                TreeNode node = root.right;
+                while (node.left != null) node = node.left;
+                node.left = root.left;
+                root = root.right;
+            }
+        }
+        return root;
+    }
+}
