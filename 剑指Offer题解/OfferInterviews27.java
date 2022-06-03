f8dd298e5497b0a99cd03af8986b0b33e1dcdbbc
iHao <sinhao@live.com>
2022-06-03 23:57:11 +0800

    OfferInterviews27【二叉树的镜像】（简单）

---
+++ 剑指Offer题解/OfferInterviews27.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
