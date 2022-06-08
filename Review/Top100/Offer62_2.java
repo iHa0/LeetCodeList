0eb96d1b06edeb08017b8293c01710098190fbe5
iHao <sinhao@live.com>
2022-06-08 22:48:36 +0800

    Offer62

---
+++ Review/Top100/Offer62_2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lastRemaining(int n, int m) {
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = (res + m) % i;
+        }
+        return res;
+    }
+}
