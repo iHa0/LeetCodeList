7e2a3cc43ea82d7d18070e5234a7feb844a87e64
iHao <sinhao@live.com>
2022-06-10 23:32:46 +0800

    LeetCode98

---
+++ LeetCode题解/Lc98.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        boolean left = isValidBST(root.left);
+        if (pre >= root.val) return false;
+        else pre = root.val;
+        boolean right = isValidBST(root.right);
+        return left && right;
+    }
+}
