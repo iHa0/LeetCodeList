1c8dab5a6d8ab48ac5b401b0205cf0f799a0ff01
iHao <sinhao@live.com>
2022-06-12 23:06:08 +0800

    LeetCode208

---
+++ Review/Top100/LeetCode208.java
@@ -0,0 +1,51 @@
+class Trie {
+    class TrieNode {
+        boolean isEnd;
+        TrieNode[] next;
+        TrieNode() {
+            isEnd = false;
+            next = new TrieNode[26];
+        }
+    }
+
+    TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) node.next[c - 'a'] = new TrieNode();
+            node = node.next[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) return false;
+            node = node.next[c - 'a'];
+        }
+        return node.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] == null) return false;
+            node = node.next[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
