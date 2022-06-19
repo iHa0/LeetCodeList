39c336739cab36de5fa334e4f3f4ca982ba181b5
iHao <sinhao@live.com>
2022-06-19 23:51:30 +0800

    CodingInterviews32

---
+++ 剑指Offer题解/CodingInterviews32(1).java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.val);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        int[] ans = new int[res.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = res.get(i);
+        }
+        return ans;
+    }
+}
