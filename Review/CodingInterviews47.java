e8b62b6e3d7e1afa9b44a6865743185b0d54bb95
iHao <sinhao@live.com>
2022-06-23 00:43:19 +0800

    CodingInterviews47

---
+++ Review/CodingInterviews47.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxValue(int[][] grid) {
+        int h = grid.length, l = grid[0].length;
+        int[][] dp = new int[h][l];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < l; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < h; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < h; i++) {
+            for (int j = 1; j < l; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[h - 1][l - 1];
+    }
+}
