94dcb48e1b08ef7d320f62ac34ed66512b91ddea
iHao <sinhao@live.com>
2022-06-23 23:14:07 +0800

    OfferInterviews33

---
+++ Review/Offer33_2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean verifyPostorder(int[] postorder) {
+        int len = postorder.length;
+        if (len < 2) return true;
+        return verify(postorder, 0, len - 1);
+    }
+
+    private boolean verify(int[] postorder, int left, int right) {
+        if (left >= right) return true;
+        int rootVal = postorder[right];
+        int k = left;
+        while (postorder[k] < rootVal) k++;
+        for (int i = k; i < right; i++) {
+            if (postorder[i] < rootVal) return false;
+        }
+        if (!verify(postorder, left, k - 1)) return false;
+        if (!verify(postorder, k, right - 1)) return false;
+        return true;
+    }
+}
