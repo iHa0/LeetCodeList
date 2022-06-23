df1bca9404abd1faef6335acb4841c3486506a4a
iHao <sinhao@live.com>
2022-06-23 23:06:54 +0800

    OfferInterviews14

---
+++ Review/Offer14_2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+}
