68d9ec5523ac5de77a28742de05229af72560683
iHao <sinhao@live.com>
2022-06-27 20:38:01 +0800

    OfferInterviews56_2

---
+++ Review/Offer56_2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return -1;
+        int[] bitSum = new int[32];
+        int res = 0;
+        for (int num : nums) {
+            int bitMask = 1;
+            for (int i = 31; i >= 0; i--) {
+                if ((num & bitMask) != 0) bitSum[i]++;
+                bitMask <<= 1;
+            }
+        }
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res += bitSum[i] % 3;
+        }
+        return res;
+    }
+}
