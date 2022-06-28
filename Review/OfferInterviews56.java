fad20df8169cba24737a0aea5a400cfcd3fb8839
iHao <sinhao@live.com>
2022-06-28 22:13:36 +0800

    OfferInterviews56

---
+++ Review/OfferInterviews56.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] singleNumbers(int[] nums) {
+        int len = nums.length, x = 0, y = 0, n = 0, m = 1;
+        for (int num : nums) n ^= num;
+        while ((n & m) == 0) m <<= 1;
+        for (int num : nums) {
+            if ((num & m) != 0) x ^= num;
+            else y ^= num;
+        }
+        return new int[] {x, y};
+    }
+}
