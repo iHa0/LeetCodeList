a0059b2b43558261d751dd51d80730e18b55469d
iHao <sinhao@live.com>
2022-07-02 23:02:50 +0800

    OfferInterviews44

---
+++ Review/Top100/OfferInterviews44.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findNthDigit(int n) {
+        int time = 1;
+        long start = 1, count =9;
+        while (n > count) {
+            n -= count;
+            time++;
+            start *= 10;
+            count = time * start * 9;
+        }
+        long num = start + (n - 1) / time;
+        return Long.toString(num).charAt((n - 1) % time) - '0';
+    }
+}
