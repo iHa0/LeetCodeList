ad94f370d69e3b7823818250f93749b8deb195b4
iHao <sinhao@live.com>
2022-07-19 22:27:22 +0800

    LeetCode135

---
+++ Review/Lc135.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int len = ratings.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+        }
+        int res = left[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) right[i] = right[i + 1] + 1;
+            res += Math.max(left[i], right[i]);
+        }
+        return res;
+    }
+}
