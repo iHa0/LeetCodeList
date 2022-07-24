f22a405feb53d3d14be4d40c4314e0fe2d28805e
iHao <sinhao@live.com>
2022-07-24 21:34:40 +0800

    LeetCode80

---
+++ Review/LeetCode80.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        if (len <= 2) return len;
+        int index = 2;
+        for (int i = 2; i < len; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
