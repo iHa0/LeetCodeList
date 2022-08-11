166740dc1ce7ea824f88aa6b96de76eb30adb327
iHao <sinhao@live.com>
2022-08-11 23:55:44 +0800

    LeetCode115

---
+++ Review/LeetCode115_2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int h = t.length(), l = s.length();
+        int[][] dp = new int[h + 1][l + 1];
+        for (int i = 0; i <= l; i++) dp[0][i] = 1;
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= l; j++) {
+                if (s.charAt(j - 1) == t.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[h][l];
+    }
+}
